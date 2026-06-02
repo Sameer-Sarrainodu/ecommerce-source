ca62971111860e4f3c3ecce4ad47bb9c27c3bf6a
Sameer-Sarrainodu <sameer339977.com>
2026-06-02 16:24:33 +0530

    ecommerce

--- mysql/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f /tmp/mysql_root_password.txt ]; then
-    PASSWORD=$(cat /tmp/mysql_root_password.txt)
-    export MYSQL_ROOT_PASSWORD=$PASSWORD
-    rm -f /tmp/mysql_root_password.txt
-fi
-
-exec /entrypoint.sh mysqld
